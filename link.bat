9aceecec9f1e42c323b679010198b7cb0811b4be
Martin Faartoft <martin.faartoft@gmail.com>
2016-07-13 22:02:37 +0200

    adding linking bat file

---
+++ link.bat
@@ -0,0 +1,4 @@
+mklink %USERPROFILE%\.rsa-unlock %~dp0.rsa-unlock
+mklink %USERPROFILE%\.bash_profile %~dp0.bash_profile
+mklink %USERPROFILE%\.bashrc %~dp0.bashrc
+mklink %USERPROFILE%\.gitconfig %~dp0.gitconfig
